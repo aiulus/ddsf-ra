0953f80f4c202efb20a800d7dd5939247b8665d2
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2024-12-06 11:54:34 +0100

    Updated the method for computing the terminal safe set

--- utils/addEquilibriumStates.m
+++ utils/addEquilibriumStates.m
@@ -1,8 +1,29 @@
 function sys = addEquilibriumStates(sys)
-    x_eq = sys.params.target;
-    u_eq = -pinv(sys.B) * sys.A * x_eq;
+    U = sys.constraints.U; % Input constraints set
+    Y = sys.constraints.Y; % Output constraints set
+
+    A = full(sys.A);
+    B = full(sys.B);
+    C = full(sys.C);
+    D = full(sys.D);
+
+    n = sys.params.n;
+
+    % x_s = sdpvar(n, 1);
+
+    T_u = pinv(B) * (eye(n) - A);
+    T_y = C + D * T_u;
+    
+    % Compute the column space of T_u
+    [U_eq, ~, ~] = svd(T_u, 'econ');
+    [Y_eq, ~, ~] = svd(T_y, 'econ');
+
+    U_s = intersect(U_eq, U);
+    Y_s = intersect(Y_eq, Y);
+
     sys.S_f = struct( ...
-        'x_eq', x_eq, ...
-        'u_eq', u_eq);
+        'U_s', U_s, ...
+        'Y_s', Y_s ...
+        );
 end
 
