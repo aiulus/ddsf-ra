02249cd73a5c5540b8425ba646ea4c5452e5977a
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2024-12-17 16:21:56 +0100

    Added rank check to Hankel matrix - generating process

--- algorithms/deepc2/deepc2_hankel.m
+++ algorithms/deepc2/deepc2_hankel.m
@@ -14,6 +14,14 @@ function H = deepc2_hankel(lookup)
         X(n*i+1:n*(i+1), :) = lookup.data.x_data(:, i+1:T-L+i+1);
     end
 
+    full_rank = PEness_check(U);
+    isPE = deepc_PEness_check(lookup.data.u_data, T_ini, T_f, lookup.sys);
+    isPE = isPE & full_rank;
+
+    if ~isPE
+        error('Persistency of excitation check failed. Please provide richer input data or adjust T_ini and N.');
+    end
+
     Up = U(1:m*T_ini, :);
     Uf = U(end -(m*T_f)+1:end, :);
     Yp = Y(1:p*T_ini, :);
