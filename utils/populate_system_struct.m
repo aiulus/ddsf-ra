31ee5afeaae6da63d933086cfa4f4aec27ddb4c9
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2024-12-18 12:52:16 +0100

    Factored out constraint handling to dedicated function

--- utils/populate_system_struct.m
+++ utils/populate_system_struct.m
@@ -10,11 +10,6 @@ function sys = populate_system_struct(A, B, C, D, params)
     sys.dims.input = size(B, 2);
     sys.dims.output = size(C, 1);
 
-    % Constraints
-    sys.constraints.U = [params.u_min, params.u_max];
-    sys.constraints.Y = [params.y_min, params.y_max];
-    %sys.constraints.U_poly = polyhe
-
     % Parameters and target
     sys.params = params;
     sys.target = params.target;
