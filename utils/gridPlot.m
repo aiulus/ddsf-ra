4c6f8a333a7dfa6a7fab374d985c27b87968076f
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-01-19 14:19:20 +0100

    gridPlot

--- utils/gridPlot.m
+++ utils/gridPlot.m
@@ -20,3 +20,36 @@ end
 function plotSingleRun(mode, data)
 end
 
+function plotdata = getDataDeePC(data, m, p, T_sim)
+    % TODO: Hash with configs
+    nruns = size(data,1);
+    t = 1:T_sim;
+    plotU = {}; plotY = {};
+    for i=1:nruns
+        row_i = data(i, :);
+        for ud=1:m
+            u_indices_d = ud + (t-1)*m;            
+            plotU{end} = u_indices_d;
+        end
+        for yd=1:p
+            y_indices_d = yd + (t-1)*p + T_sim*m;  
+            plotY{end} = y_indices_d;
+        end
+    end
+    plotdata = struct('u', plotU, 'y', plotY);
+end
+
+function plotdata = getDataDDSF(m, T_sim)
+    nruns = size(data,1);
+    t = 1:T_sim;
+    plotdata = {}; 
+    for i=1:nruns
+        row_i = data(i, :);
+        for ud=1:m
+            u_indices_d = ud + (t-1)*m;          
+            ul_indices_d = uld + (t-1)*m + T_sim*m;  
+            plotdata{end} = [u_indices_d,  ul_indices_d];
+        end
+    end
+
+end
